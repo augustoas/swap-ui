9f93d1313258744eabdfc5b9edb0b983ac975133
Augusto Alvarez Smith <augustoalvarezsmith@Augustos-MacBook-Pro.local>
2024-04-14 18:32:19 -0400

    add folders

--- swap-ui/src/components.d.ts
+++ swap-ui/src/components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./components/AppFooter.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
+    NewJobForm: typeof import('./components/Forms/NewJobForm/NewJobForm.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Topbar: typeof import('./components/Navigation/Topbar/Topbar.vue')['default']
