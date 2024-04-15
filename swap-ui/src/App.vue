632a7a1de46ce6c639161f9b4af9e55fe21244eb
Ignacio Zenteno Smith <izenteno22@gmail.com>
2024-04-14 20:10:51 -0400

    fixes

--- swap-ui/src/App.vue
+++ swap-ui/src/App.vue
@@ -1,11 +1,43 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+  <v-app id="app">
+    <div class="app-container">
+      <TopBar />
+      <v-main class="main-class">
+        <router-view />
+      </v-main>
+      <BottomBar />
+    </div>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import Vue from "vue";
+import TopBar from "./components/Navigation/TopBar/TopBar.vue"
+import BottomBar from "./components/Navigation/BottomBar/BottomBar.vue";
+
 </script>
+
+<style lang="scss">
+@import "styles.scss";
+
+.main-class {
+  margin-top: 110px; /* height of the TopBar */
+  margin-bottom: 60px; /* height of the BottomBar */
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  // Hide scrollbar if it's not needed
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.app-container {
+  background-color: rebeccapurple;
+}
+</style>
